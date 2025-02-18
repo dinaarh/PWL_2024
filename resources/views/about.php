2470a05961ca2f181479ce816f0df79ed7707578
dinaarh <rahmawatidina0404@gmail.com>
2025-02-18 10:00:40 +0700

    Jb 2_Praktikum 1_langkah 7-route /about

---
+++ resources/views/about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About Dina</title>
+        <style>
+            body {
+                font-family: sans-serif;
+                background-color: #F0F8FF;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                text-align: left;
+            }
+            .box {
+                background: #ADD8E6;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            }
+            h2 {
+                color: black;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="box">
+            <h2>NIM  : 2341720050</h2>
+            <h2>Nama : Dina Rahmawati</h2>
+        </div>
+    </body>
+</html>
