979e6ff8d7a9b8d8c67dd6cf2e0dce70abc610f7
dinaarh <rahmawatidina0404@gmail.com>
2025-02-18 10:48:11 +0700

    Jb 2_Praktikum 1_langkah 14-Optional Parameters  /user

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::get('/articles/{id}', function ($articlesId) {
     return 'Halaman artikel dengan ID ' . $articlesId;
 });
 
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya ' . $name;
+});
+
