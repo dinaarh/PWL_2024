7380334ba847fba61ee4e4024d20557a8213ac94
dinaarh <rahmawatidina0404@gmail.com>
2025-02-18 15:09:51 +0700

    Jb 2_Praktikum 2_langkah 11-route crud PhotoController

--- routes/web.php
+++ routes/web.php
@@ -62,4 +62,10 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
-Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
